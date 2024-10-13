1978a531f630cc21564e635ad5d3eedebac1376f
Kekwy <keekkewy@qq.com>
2024-10-13 19:21:44 +0800

    feat: 完成基本功能

--- src/main/java/com/kekwy/nmo/cmdtgr/Main.java
+++ src/main/java/com/kekwy/nmo/cmdtgr/Main.java
@@ -11,12 +11,18 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        getServer().getMessenger() // 注册通道
+                .registerOutgoingPluginChannel(this, "cmdtgr:channel");
+
         /* 注册指令，并指定指令处理器 */
-        Objects.requireNonNull(this.getCommand("cmdtgr")).setExecutor(new CmdtgrExecutor());
+        Objects.requireNonNull(this.getCommand("cmdtgr")).setExecutor(
+                new CmdtgrExecutor(getLogger(), this)
+        );
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        this.getServer().getScheduler().cancelTasks(this);
     }
 }
--- src/main/java/com/kekwy/nmo/cmdtgr/executor/CmdtgrExecutor.java
+++ src/main/java/com/kekwy/nmo/cmdtgr/executor/CmdtgrExecutor.java
@@ -1,12 +1,28 @@
 package com.kekwy.nmo.cmdtgr.executor;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class CmdtgrExecutor implements CommandExecutor {
+
+    private final Logger logger;
+    private final Plugin plugin;
+
+    public CmdtgrExecutor(Logger logger, Plugin plugin) {
+        this.logger = logger;
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
@@ -15,13 +31,22 @@ public class CmdtgrExecutor implements CommandExecutor {
             return false;
         }
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            player.sendMessage("You executed the custom command!");
-            return true;
+        String playerName = args[0];
+        Player player = Bukkit.getPlayerExact(playerName);
+
+        if (player != null && player.isOnline()) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 1; i < args.length; i++) {
+                builder.append(args[i]);
+                builder.append(" ");
+            }
+            player.sendPluginMessage(plugin,  // 使用自定义消息通道发送消息到指定玩家
+                    "cmdtgr:channel", builder.toString().getBytes());
+        } else {
+            sender.sendMessage("Player not found or not online.");
         }
-        sender.sendMessage("This command can only be executed by a player!");
-        return false;
+
+        return true;
     }
 
 }
